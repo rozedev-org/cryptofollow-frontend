3848e17c86c7de69072933a19863558c3f26829b
Juan C Jimenez <juancarlo14071@gmail.com>
2025-02-03 11:45:55 -0300

    Refactor layout components in Login and Wallet pages for improved structure and responsiveness

--- src/components/layout/base-page/base-page.tsx
+++ src/components/layout/base-page/base-page.tsx
@@ -4,11 +4,11 @@ interface BasePageProps extends FlexProps {}
 export const BasePage = (props: BasePageProps) => {
   return (
     <Flex
-      {...props}
       w={"100%"}
       data-component={"base-page"}
       overflowX="auto"
       p={[2, 0]}
+      {...props}
     >
       {props.children}
     </Flex>
