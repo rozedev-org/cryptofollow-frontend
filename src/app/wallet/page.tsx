f1a65df69edab114e58034c729db458d1a46b825
Juan C Jimenez <juancarlo14071@gmail.com>
2025-05-09 14:44:47 -0400

    fix: add revalidate constant to Dashboard and WalletPage components

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,6 +1,8 @@
 import { BasePage } from "@/components/layout/base-page/base-page";
 import { WalletTable } from "./components/WalletTable";
 
+export const revalidate = 0;
+
 export default function WalletPage() {
   return (
     <BasePage>
